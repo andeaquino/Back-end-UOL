4778eba12a50539931287f7fd34c1d758d39b4eb
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-10-11 07:52:06 -0300

    Get and post of '/participants' route complete

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import express from "express";
 import cors from 'cors';
 
+const participants = [];
+const messages = [];
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post();
+app.post('/participants', (req, res) => {
+    const name = req.body.name;
+    const isUsed = participants.find(participant => participant.name === name);
+    if (!!isUsed || name === "") {
+        res.status(400).send("Invalid name");
+    } else {
+        participants.push(
+            {
+                name,
+                lastStatus: Date.now()
+            }
+        );
+        res.status(200).send("Name saved");
+    }
+});
 
-app.get();
+app.get('/participants', (req, res) => {
+    res.send(participants);
+});
 
-app.ṕost();
 
-app.get();
-
-app.ṕost();
 
 app.listen(4000);
