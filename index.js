a14379bba8db1c72fc9699e1f71f8235e4cdb8a9
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-10-11 08:54:34 -0300

    Get and post of '/messages' route complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import dayjs from "dayjs";
 
 const participants = [];
 const messages = [];
@@ -11,6 +12,7 @@ app.use(express.json());
 app.post('/participants', (req, res) => {
     const name = req.body.name;
     const isUsed = participants.find(participant => participant.name === name);
+
     if (!!isUsed || name === "") {
         res.status(400).send("Invalid name");
     } else {
@@ -28,6 +30,39 @@ app.get('/participants', (req, res) => {
     res.send(participants);
 });
 
+app.ṕost('/messages', (req, res) => {
+    const message = req.body;
+    const user = req.header(User);
+    const isEmpty = message.to === "" || message.text === "";
+    const isTypeCorrect = message.type === 'message' || message.type === 'private_message';
+    const isUserOn = participants.find(participant => participant.name === user);
 
+    if (isEmpty || !isTypeCorrect || !isUserOn) {
+        res.status(400).send("Message not sent");
+    } else {
+        messages.push({
+            ...message,
+            from: user,
+            time: Date.now().format('HH:MM:SS')
+        });
+        res.status(200).send("Message sent");
+    }
+});
+
+app.get('/messages', (req, res) => {
+    const limit = req.query.limit ? req.query.limit : 0;
+    const user = req.header(User);
+    const userMessages = [];
+    messages.map(message => {
+        if (message.type === 'message') {
+            userMessages.push(message);
+        } else {
+            if (message.to === user || message.from === user) {
+                userMessages.push(message);
+            }
+        }
+    });
+    res.status(200).send(userMessages.slice(Math.max(userMessages.length - limit, 0)));
+});
 
 app.listen(4000);
