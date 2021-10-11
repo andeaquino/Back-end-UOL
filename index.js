e8dfdfe223623f65171b6e645e633c07da61a992
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-10-11 07:24:45 -0300

    server initialized

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post();
+
+app.get();
+
+app.ṕost();
+
+app.get();
+
+app.ṕost();
+
+app.listen(4000);
